f68530a4c3322c103b241db6e00f8d1f5d70980d
Ujjwal jain <ujjwaljainmain@gmail.com>
2024-07-28 23:32:41 +0530

    Add two.js

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+//while loop
+
+let index = 0
+while(index <= 10){
+    console.log(`value of index is ${index}`);
+    index = index +2
+}
+
+let array = ['flash','batman','superman']
+let arr = 0
+while(arr<array.length){
+    console.log(`value is ${array[arr]}`);
+    arr++
+}
+
+//first execute code then checks the condition
+let score = 1
+
+do{
+    console.log(`score is ${score}`);
+    score++ 
+}while(score <= 10)
