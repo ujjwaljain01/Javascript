2b06dd1bab1a505898961e96611121f6dd3a5659
Ujjwal jain <ujjwaljainmain@gmail.com>
2024-09-13 20:41:32 +0530

    Add Project_5 - Display Key pressed

---
+++ 09_async_projects/Project_5-Key_press/key.js
@@ -0,0 +1,29 @@
+//My approach(little bit of peeking)
+const insertt = document.querySelector('#insert')
+window.addEventListener('keydown',function(e){
+    insertt.innerHTML = `<h2>${e.key}</h2>`
+})
+
+//sir's appraoch
+const insert = document.getElementById('insert');
+
+window.addEventListener('keydown', (e) => {
+  insert.innerHTML = `
+    <div class='color'>
+    <table>
+    <tr>
+      <th>Key</th>
+      <th>Keycode</th> 
+      <th>Code</th>
+    </tr>
+    <tr>
+      <td>${e.key === ' ' ? 'Space' : e.key}</td>
+      <td>${e.keyCode}</td> 
+      <td>${e.code}</td>
+    </tr>
+    
+  </table>
+    </div>
+  `;
+});
+
